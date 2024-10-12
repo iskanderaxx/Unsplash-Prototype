bdd66708be5c9a3fd052055effe492a9a2c41833
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-10-12 15:08:19 +0400

    Initial Commit

---
+++ Unsplash Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash Prototype
+//
+//  Created by Mac Alexander on 12.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
